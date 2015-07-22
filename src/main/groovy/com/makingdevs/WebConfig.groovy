805b50388a9f46b9f5b5f778eb89c2e379391b52
sohjiro <felipe@makingdevs.com>
2015-07-22 14:15:39 -0500

    modificando configuraciones y agregando el component scan

--- src/main/groovy/com/makingdevs/WebConfig.groovy
+++ src/main/groovy/com/makingdevs/WebConfig.groovy
@@ -1,13 +1,39 @@
 package com.makingdevs
 
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.web.servlet.ViewResolver
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.view.InternalResourceViewResolver
+import org.springframework.web.servlet.view.JstlView
 
 @Configuration
+@ComponentScan(basePackages = ["com.makingdevs"])
 @EnableWebMvc
-@ComponentScan("com.makingdevs")
-public class AppConfig extends WebMvcConfigurerAdapter {
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/static/**").addResourceLocations("/libs/")
+  }
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/").setViewName("home")
+  }
+
+  @Bean
+  public ViewResolver viewResolver(){
+    InternalResourceViewResolver resolver = new InternalResourceViewResolver()
+    resolver.setViewClass(JstlView.class)
+    resolver.setPrefix("/WEB-INF/jsp/")
+    resolver.setSuffix(".jsp")
+    return resolver
+  }
 
 }
